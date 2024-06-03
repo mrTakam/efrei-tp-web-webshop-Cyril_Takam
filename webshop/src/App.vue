d0e8badd5d31e66a721276d1a124d7acf3e68211
helios_IT <mr.takamcyril@gmail.com>
2024-06-03 21:41:59 +0200

    Update all files with the latest changes

---
+++ webshop/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app" class="container">
+      <Menu></Menu>
+      <IndexPage></IndexPage>
+    </div>
+  </template>
+  
+  <script>
+  
+  import Menu from "./components/Menu";
+  import IndexPage from "./components/IndexPage";
+  export default {
+    name: 'App',
+    components: {
+      IndexPage,
+      Menu
+    }
+  }
+  </script>
+  
+  <style>
+  </style>
+  
